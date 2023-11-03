47f9819d3711c0152e46efff48ca0f7e314e5b2d
inesgpinto <inesgpinto@tecnico.ulisboa.pt>
2023-11-03 19:38:02 +0000

    part1 done

--- mycode/part1.py
+++ mycode/part1.py
@@ -37,17 +37,55 @@ c_obs = interpolate.interp1d( x = ssp_obs[" r/R_sun"],
                              bounds_error=False  # Allow extrapolation
                              )
 
-x = np.linspace(model_gs98["radius"].min(), model_gs98["radius"].max(),100)
+x = np.linspace(ssp_obs[" r/R_sun"].min(), ssp_obs[" r/R_sun"].max(),100)
 y = c_obs(x)
 
 plt.plot( ssp_obs[" r/R_sun"], ssp_obs["c [cm/s]"],label = "obs points",marker="o", linestyle = 'None')
 plt.plot(x,y,label='obs interpol cubic')
 plt.plot(model_gs98["radius"],model_gs98["csound"], label = "gs98 model",marker=".",linestyle = 'None')
 plt.legend(numpoints=1)
+plt.ylabel(r"$c$ $[cm/s]$")
+plt.xlabel(r"$R/R_{sun}$")
+plt.title("Sound Speed Profile")
 plt.show()
 
 
-# %%
+# %% [markdown]
+# # Comparison of Sound Speed Profiles
 
+# %%
+# calcular sigmac/c num loop para ter a certeza da correspondencia entre raio e velocidade
+
+deltcc_gs98 = ( c_obs(model_gs98["radius"]) - model_gs98["csound"] ) / model_gs98["csound"]
+
+plt.axhline(0,color='grey', linestyle='dashed')
+plt.plot(model_gs98["radius"],deltcc_gs98, label= "gs98" )
+plt.legend()
+plt.ylabel(r"$\delta c /c$")
+plt.xlabel(r"$R/R_{sun}$")
+plt.show()
+
+# %% [markdown]
+# now, choose model closer to 0 ...
+# 
+# find a measure for this...
+
+# %% [markdown]
+# # Plot frequency vs radial order l plane
+
+# %%
+freqs_gs98_path  = "../solar_23.05.1/freqs_summary.txt"
+column_names = ["Re(freq)", "Im(freq)", "l", "n_g", "n_p", "n_pg"]
+freqs_gs98 = pd.read_csv(freqs_gs98_path, sep='\s+', header=None, names=column_names, skiprows=6)
+print(freqs_gs98)
+
+# %%
+for l in freqs_gs98["l"].unique():
+    plt.plot(freqs_gs98.query(f" l == {l}")["n_p"], freqs_gs98.query(f" l == {l}")["Re(freq)"], label = f"l ={l}")
+    
+plt.legend()
+plt.xlabel("Radial Order")
+plt.ylabel("Frequency")
+plt.show()
 
 
